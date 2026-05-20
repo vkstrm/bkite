b6db32cee3b6f4c0abbf987023c9b4d5c4421419
vkstrm <vkstrm@fastmail.com>
2026-05-20 20:28:44 +0200

    test sh

---
+++ test.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+REPO="vkstrm-co/stuff"
+IMAGE="packages.buildkite.com/vkstrm-co/stuff/gotest:latest"
+
+if [[ $# == 0 ]]; then
+  echo "Requires arguments"
+  exit 0
+fi
+
+buildkite-agent oidc request-token --audience "https://packages.buildkite.com/${REPO}" --lifetime 300 | docker login packages.buildkite.com/${REPO} --username buildkite --password-stdin
+docker pull $IMAGE
+docker run --rm -v $(pwd):$(pwd) -w $(pwd) $IMAGE "$@"
+
+curl \
+  -X POST \
+  --fail-with-body \
+  -H "Authorization: Token token=\"$BUILDKITE_ANALYTICS_TOKEN\"" \
+  -F "data=@junit.xml" \
+  -F "format=junit" \
+  -F "run_env[CI]=buildkite" \
+  -F "run_env[key]=$BUILDKITE_BUILD_ID" \
+  -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
+  -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
+  -F "run_env[branch]=$BUILDKITE_BRANCH" \
+  -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
+  -F "run_env[message]=$BUILDKITE_MESSAGE" \
+  -F "run_env[url]=$BUILDKITE_BUILD_URL" \
+  https://analytics-api.buildkite.com/v1/uploads
+
