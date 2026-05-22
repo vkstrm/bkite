90e43766fa0b65eed841888eb3db7e95bf4a0223
vkstrm <vkstrm@fastmail.com>
2026-05-22 16:16:39 +0200

    dynamic pipe

--- pipeline.sh
+++ pipeline.sh
@@ -7,7 +7,8 @@ IMAGE="packages.buildkite.com/${REPO}/bkite:latest"
 
 buildkite-agent oidc request-token --audience "https://packages.buildkite.com/${REPO}" --lifetime 300 | docker login packages.buildkite.com/${REPO} --username buildkite --password-stdin
 docker pull $IMAGE
-docker run --rm -v $(pwd):$(pwd) -w $(pwd) $IMAGE go run pipeline/cmd/pipeline.go > pipeline.yaml
+docker run --rm -v $(pwd):$(pwd) -w $(pwd) $IMAGE go run pipeline/cmd/pipeline.go > custom-pipe.yaml
 
-# go run pipeline/cmd/pipeline.go > pipeline.yaml
-buildkite-agent pipeline upload pipeline.yaml
+cat custom-pipe.yaml
+
+buildkite-agent pipeline upload custom-pipe.yaml
