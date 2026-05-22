d6f0bf5c97bcc58a5e912d3cc4142422fc7e72df
vkstrm <vkstrm@fastmail.com>
2026-05-22 16:14:43 +0200

    fix

--- pipeline.sh
+++ pipeline.sh
@@ -5,11 +5,6 @@ set -euo pipefail
 REPO="vkstrm-co/stuff"
 IMAGE="packages.buildkite.com/${REPO}/bkite:latest"
 
-if [[ $# == 0 ]]; then
-  echo "Requires arguments"
-  exit 0
-fi
-
 buildkite-agent oidc request-token --audience "https://packages.buildkite.com/${REPO}" --lifetime 300 | docker login packages.buildkite.com/${REPO} --username buildkite --password-stdin
 docker pull $IMAGE
 docker run --rm -v $(pwd):$(pwd) -w $(pwd) $IMAGE go run pipeline/cmd/pipeline.go > pipeline.yaml
