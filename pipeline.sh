227c925f9e58dda3db40996e60386c2b149726e6
vkstrm <vkstrm@fastmail.com>
2026-05-23 23:36:36 +0200

    env vars

--- pipeline.sh
+++ pipeline.sh
@@ -5,9 +5,12 @@ set -euo pipefail
 REPO="vkstrm-co/stuff"
 IMAGE="packages.buildkite.com/${REPO}/bkite:latest"
 
+# Propagate env variables
+env | grep BUILDKITE > env.list
+
 buildkite-agent oidc request-token --audience "https://packages.buildkite.com/${REPO}" --lifetime 300 | docker login packages.buildkite.com/${REPO} --username buildkite --password-stdin
 docker pull $IMAGE
-docker run --rm -v $(pwd):$(pwd) -w $(pwd) $IMAGE go run pipeline/cmd/pipeline.go > custom-pipe.yaml
+docker run --rm -v $(pwd):$(pwd) -w $(pwd) --env-file env.list $IMAGE go run pipeline/cmd/pipeline.go > custom-pipe.yaml
 
 cat custom-pipe.yaml
 
